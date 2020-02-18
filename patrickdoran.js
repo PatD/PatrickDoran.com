d63a9e136d3a0922053981e70b015a6dbc755729
Patrick Doran <pdoran@gmail.com>
2020-02-17 20:55:25 -0500

    Clouds

---
+++ patrickdoran.js
@@ -0,0 +1,26 @@
+// Patrickdoran.js
+/*
+
+ <div id="cloud-circle"></div>
+ 
+  <svg width="0" height="0">
+    <filter id="filter">
+      <feTurbulence type="fractalNoise" baseFrequency=".012" numOctaves="10"  />
+      <feDisplacementMap in="SourceGraphic" scale="180"  />
+    </filter>
+  </svg>
+
+*/
+
+
+// Pass random number, 1-3
+
+// Pass random seeds
+
+// pass random postions
+
+function happyClouds(){
+  
+  
+  
+};
