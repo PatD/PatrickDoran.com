2f20f98bd862279aac34fd206b30aebd85e5aef4
PatD <pdoran@gmail.com>
2020-06-10 19:24:17 -0400

    Fix SW cahce

--- service-worker.js
+++ service-worker.js
@@ -15,14 +15,12 @@ var urlsToCache = [
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME).then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    const cache = await caches.open(CACHE_NAME)
 
     // Setting {cache: 'reload'} in the new request will ensure that the response
     // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
     await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+    await cache.add(new Request(urlsToCache, {cache: 'reload'}));
   })());
 });
 
